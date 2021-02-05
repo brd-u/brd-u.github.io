8337e01124a2c6eb85cd379d370f395e307eef48
brd-u <malinowyjan12@gmail.com>
2021-02-05 13:39:33 +0100

    Updated tiles

--- src/app/components/Work.js
+++ src/app/components/Work.js
@@ -3,6 +3,7 @@ import './main.css'
 import Tile from './Tile.js'
 // import BarChart from './BarChart.js'
 import focusTimerImg from '../assets/focus.png'
+import rogueGameImg from '../assets/rogue.png'
 import Modal from './modals/testResultModal.js'
 import { NavLink } from 'react-router-dom'
 // import { Link } from 'react-router-dom'
@@ -41,10 +42,15 @@ class Work extends Component {
     }
 
 
-goToFocusTimerPage=()=>{
+    goToFocusTimerPage = () => {
 
-    window.location.assign('https://brd-u.github.io/focus-timer/');
-}
+        window.location.assign('https://brd-u.github.io/focus-timer/');
+    }
+
+    goToRoguelikeGamePage = () => {
+
+        window.location.assign('https://brd-u.github.io/roguelike-game/');
+    }
 
     render() {
         return (
@@ -59,7 +65,7 @@ goToFocusTimerPage=()=>{
                             My <span className='text-secondary'>Work</span>
                         </h1>
                         <h2 className='sm-heading'>
-                            Tutaj jakiś tekst
+                            Check out some of my projects...
                             <div>
                                 {/* <input onChange={this.handleDataChange.bind(this)} type='text' ref='number' ></input> */}
                             </div>
@@ -67,11 +73,12 @@ goToFocusTimerPage=()=>{
 
                         </h2>
 
-                        <div>
+                        <div >
 
                             <Tile onClick={this.openModalHandler.bind(this)} className='tile-1' imgSrc='https://avatars3.githubusercontent.com/u/12559275?s=400&v=4' title='Nigchtwatch.js' header='Tutaj jakieś coś' mainText='Bacon ipsum dolor amet pork belly tri-tip turducken, pancetta bresaola pork chick ipsum dolor amet pork belly tri-tip turducken, pancetta bresaola pork chicken meatloaf. Flank sirloin strip steak prosciutto kevin turduckenen meatloaf. Flank sirloin strip steak prosciutto kevin turducken.' />
                             <Tile onClick={this.goToFocusTimerPage.bind(this)} className='tile-2' imgSrc={focusTimerImg} title='Focus Timer' header='Tutaj jakieś coś' mainText='timerducken, pancetta bresaola pork chick ipsum dolor amet pork belly tri-tip turducken, pancetta bresaola pork chicken mea' />
-
+                            <Tile onClick={this.goToRoguelikeGamePage.bind(this)} className='tile-3' imgSrc={rogueGameImg} title='Roguelike game' header='Tutaj jakieś coś' mainText='timerducken, pancetta bresaola pork chick ipsum dolor amet pork belly tri-tip turducken, pancetta bresaola pork chicken mea' />
+                            <Tile onClick={this.goToRoguelikeGamePage.bind(this)} className='tile-4' imgSrc={rogueGameImg} title='Roguelike game' header='Tutaj jakieś coś' mainText='timerducken, pancetta bresaola pork chick ipsum dolor amet pork belly tri-tip turducken, pancetta bresaola pork chicken mea' />
                         </div>
 
 
