3fe130f575df2e5f1564ce26b62a2139313e2644
brd-u <malinowyjan12@gmail.com>
2021-05-07 13:47:19 +0200

    Updated Tile (git,report)

--- src/app/components/js/Tile.js
+++ src/app/components/js/Tile.js
@@ -1,19 +1,48 @@
 import '../css/Tile.css'
-
+import { SiGithub } from 'react-icons/si'
+import { HiDocumentReport } from 'react-icons/hi'
+import React, { useState } from 'react';
+import Modal from '../modals/testResultModal.js'
 
 const Tile = (props) => {
-    
+
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+
+    const setModalIsOpenToTrue = () => {
+        setModalIsOpen(true)
+    }
+
+    const setModalIsOpenToFalse = () => {
+        setModalIsOpen(false)
+    }
+
+
+
     return (
-        // <div className="wrap">
-            <div className={`tile ${props.className}`} onClick={props.onClick}>
-                <img src={`${props.imgSrc}`} alt='company img'/>
+        <div className="wrap">
+            <div className={`tile ${props.className}`} >
+                <img src={`${props.imgSrc}`} alt='company img' />
+
                 <div className="text">
                     <h1>{props.title}</h1>
-                    <h2 className="animate-text">{props.header}</h2>
-                    <p className="animate-text">{props.mainText}</p>
+                    <h2 className="andimate-text">{props.header}</h2>
+                    <p >{props.firstPar}</p>
+                    <p>{props.secPar}</p>
+                    <p className="tag-paragraf">{props.tags}</p>
                 </div>
+
+                <div className='animate-info-icons'>
+                    <a href='https://github.com/brd-u/cypress' className='social-icon-link' target='_blank' rel="noopener noreferrer">
+                        <SiGithub className='info-icon' />
+                    </a>
+                        <HiDocumentReport onClick={setModalIsOpenToTrue} className='info-icon' />
+                </div>
+
+                {/* <button onClick={setModalIsOpenToFalse}>x</button> */}
+                {/* <AnimeList/> */}
             </div>
-        // </div>
+            <Modal show={modalIsOpen} close={setModalIsOpenToFalse} />
+        </div>
     );
 };
 
