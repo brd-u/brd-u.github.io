a31289aebbc5bbde3b08bdc8f45ecf304853f767
brd-u <malinowyjan12@gmail.com>
2021-05-07 13:48:17 +0200

    Added new tile(cypress)

--- src/app/components/js/Work.js
+++ src/app/components/js/Work.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 import '../css/main.css'
 import Tile from './Tile.js'
-import focusTimerImg from '../../assets/focus.png'
-import rogueGameImg from '../../assets/rogue.png'
+// import focusTimerImg from '../../assets/focus.png'
+// import rogueGameImg from '../../assets/rogue.png'
 import Modal from '../modals/testResultModal.js'
 // import { NavLink } from 'react-router-dom'
 // import { Link } from 'react-router-dom'
@@ -64,11 +64,11 @@ class Work extends Component {
                             My <span className='text-secondary'>Work</span>
                         </h1>
                         <h2 className='sm-heading'>Check out some of my projects... </h2>
-                        <h1 className='sm-heading' >In progress...</h1>
+                        {/* <h1 className='sm-heading' >In progress...</h1> */}
                         <div >
 
-                            {/* <Tile onClick={this.openModalHandler.bind(this)} className='tile-1' imgSrc='https://avatars3.githubusercontent.com/u/12559275?s=400&v=4' title='Nigchtwatch.js' header='Tutaj jakieś coś' mainText='Bacon ipsum dolor amet pork belly tri-tip turducken, pancetta bresaola pork chick ipsum dolor amet pork belly tri-tip turducken, pancetta bresaola pork chicken meatloaf. Flank sirloin strip steak prosciutto kevin turduckenen meatloaf. Flank sirloin strip steak prosciutto kevin turducken.' />
-                            <Tile onClick={this.goToFocusTimerPage.bind(this)} className='tile-2' imgSrc={focusTimerImg} title='Focus Timer' header='Tutaj jakieś coś' mainText='timerducken, pancetta bresaola pork chick ipsum dolor amet pork belly tri-tip turducken, pancetta bresaola pork chicken mea' />
+                            <Tile className='tile-1' imgSrc='https://media-exp1.licdn.com/dms/image/C4E0BAQGhE8jNwjlc3w/company-logo_200_200/0/1554836371931?e=2159024400&v=beta&t=tQbdczcJ9C9g1pggtEXpC5CQgaFxCZn_d3CAOyeWWDU' title='Cypress' header='Sample of Automation Test Suite' firstPar='WEB: SHOP.DEMOQA.COM' secPar='Checking the basic functionalities of the test page.' tags='[ hooks, fixtures, commands, pop, env var, app actions, mochawesome report, data generators(credit card, data, id, pesel) ]' />
+                            {/* <Tile onClick={this.goToFocusTimerPage.bind(this)} className='tile-2' imgSrc={focusTimerImg} title='Focus Timer' header='Tutaj jakieś coś' mainText='timerducken, pancetta bresaola pork chick ipsum dolor amet pork belly tri-tip turducken, pancetta bresaola pork chicken mea' />
                             <Tile onClick={this.goToRoguelikeGamePage.bind(this)} className='tile-3' imgSrc={rogueGameImg} title='Roguelike game' header='Tutaj jakieś coś' mainText='timerducken, pancetta bresaola pork chick ipsum dolor amet pork belly tri-tip turducken, pancetta bresaola pork chicken mea' /> */}
                             {/* <Tile onClick={this.goToRoguelikeGamePage.bind(this)} className='tile-4' imgSrc={rogueGameImg} title='Roguelike game' header='Tutaj jakieś coś' mainText='timerducken, pancetta bresaola pork chick ipsum dolor amet pork belly tri-tip turducken, pancetta bresaola pork chicken mea' /> */}
                         </div>
